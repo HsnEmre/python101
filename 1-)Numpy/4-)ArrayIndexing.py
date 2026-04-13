6f590414c613745708efd1cec95503bdc63f41db
Hasan Emre Buzkan <92093161+HsnEmre@users.noreply.github.com>
2026-04-13 09:59:40 +0300

    indexing

---
+++ 1-)Numpy/4-)ArrayIndexing.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+array=np.array([10,20,30,40,50])
+
+
+eleman=array[2]
+print(eleman)
+
+
+#if you wanna multi dimensional seriaes
+matris=np.array([[1,2,3],[4,5,6],[7,8,9]])
+eleman2=matris[1,2]
+
+print(eleman2)
