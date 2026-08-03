1738b488e9bf0e7490c90ca8a2e92783d8977693
Hasan Emre Buzkan <92093161+HsnEmre@users.noreply.github.com>
2026-08-03 12:35:59 +0300

    c

--- 5-)Veri Ön İşleme Mühendisliği/veriönizleme.py
+++ 5-)Veri Ön İşleme Mühendisliği/veriönizleme.py
@@ -1,5 +1,5 @@
 ﻿import pandas as pd
-
+from sklearn.preprocessing import LabelEncoder
 
 #veriyi yukle 
 df=pd.read_excel('veri_on_isleme_ve_ozellik_muhendisligi.xlsx')
@@ -10,6 +10,10 @@ df('Gelir').fillna(df['Gelir'].mean(),inplace=True)
 
 print(df)
 
+
+le=LabelEncoder()
+df['Cinsiyet']=le.fit_transform(df['cinsiyet'])
+print(df)
 
 
 
